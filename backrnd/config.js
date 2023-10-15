1898b8d6468dca46aa7599ffa5d8dc1bf38ef8d1
Zahra Sabri <zahrasabri79@gmail.com>
2023-10-15 09:30:50 +0330

    building all files and folders

---
+++ backrnd/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+export const mongodbURL =
+  "mongodb+srv://zahra:9QSv5a9R3cQKpWLX@cluster0.wikejc4.mongodb.net/books-collection?retryWrites=true&w=majority";
---
+++ frontend/src/pages/editBook.jsx
@@ -0,0 +1,7 @@
+// import React from 'react'
+
+const EditBook = () => {
+  return <div>edit</div>;
+};
+
+export default EditBook;
